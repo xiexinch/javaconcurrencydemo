fc1b1749d3a97ce71e3e0fd8c236ca3227cb7859
xiexinch <test767803@foxmail.com>
2020-05-03 16:17:49 +0800

    线程上下文类加载器

--- src/main/java/chapter11/MainThreadClassLoader.java
+++ src/main/java/chapter11/MainThreadClassLoader.java
@@ -1,4 +1,8 @@
 package chapter11;
+import static java.lang.Thread.currentThread;
 
 public class MainThreadClassLoader {
+    public static void main(String[] args) {
+        System.out.println(currentThread().getContextClassLoader());
+    }
 }
